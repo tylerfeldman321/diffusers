b282df1d84e761859e9c159804092816e0968acd
Paul Truitt <ptruitt7@gmail.com>
2023-04-19 15:19:55 -0400

    Chainging script

--- pauls_test_ip2p.py
+++ pauls_test_ip2p.py
@@ -6,6 +6,7 @@ import sys
 import torch
 from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
 from PIL import Image
+import numpy as np
 
 model_id = 'timbrooks/instruct-pix2pix'
 
@@ -13,8 +14,15 @@ pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dt
 pipe.to('cuda')
 pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
 img = Image.open(sys.argv[1])
-mask = Image.open(sys.argv[2])
-images = pipe(sys.argv[3], image=img, mask=mask, mask_guidance_scale=0.2).images
+mask_init = Image.open(sys.argv[2])
+mask_scale = float(sys.argv[5])
+mask_guidance_scale = float(sys.argv[6])
+mask_frequency = int(sys.argv[7])
+mask_numpy = np.array(mask_init)
+mask_int = mask_numpy.astype(int)
+mask = mask_int / mask_int.max()
+images = pipe(sys.argv[3], image=img, mask=mask, mask_guidance_scale=mask_scale,
+              guidance_scale=mask_guidance_scale, mask_enforcement_frequency=mask_frequency).images
 result = images[0]
 
 result.save(sys.argv[4])
