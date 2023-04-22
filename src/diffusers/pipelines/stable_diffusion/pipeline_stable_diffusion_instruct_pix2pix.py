82c8715b0a08de0725f9d0dd8e153ee12b6ff4fb
Paul Truitt <ptruitt7@gmail.com>
2023-04-22 10:54:20 -0400

    Printing mask info

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_instruct_pix2pix.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_instruct_pix2pix.py
@@ -676,7 +676,10 @@ class StableDiffusionInstructPix2PixPipeline(DiffusionPipeline, TextualInversion
         # We will want mask to be 1s where it's okay to change
         original_image_numpy = np.asarray(original_image.cpu())
         current_image_numpy = np.asarray(current_image.cpu())
+        print(type(mask))
+        print(mask.shape)
         mask_numpy = np.asarray(mask)
+        print(mask_numpy.shape)
         inter = np.array([mask_numpy])
         print(inter.shape)
         mask_numpy = inter.transpose(0, 3, 1, 2)
