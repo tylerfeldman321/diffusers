c4192c550d396c9a99cbde43d663e8a22821bb0d
Tyler Feldman <tylerfeldman321@gmail.com>
2023-04-24 14:30:09 -0400

    Automatically generate output image paths

--- results_generation_data.py
+++ results_generation_data.py
@@ -1,23 +1,30 @@
-INPUT_IMAGE = ['apple_man.jpg', 'apple_man.jpg', 'apple_man.jpg',
-               'dog_on_bench.png', 'dog_on_bench.png', 'dog_on_bench.png',
-               'flowers.jpg', 'flowers.jpg', 'flowers.jpg',
-               'lebron.jpeg', 'lebron.jpeg', 'lebron.jpeg',
-               'london.jpg', 'london.jpg', 'london.jpg',
-               'statue.jpeg', 'statue.jpeg', 'statue.jpeg',
-               'sunflower.jpg', 'sunflower.jpg', 'sunflower.jpg',
-               'tree_and_building.png', 'tree_and_building.png', 'tree_and_building.png',
-               'two_men_on_bench.png', 'two_men_on_bench.png', 'two_men_on_bench.png',
-               'woman_and_fence.png', 'woman_and_fence.png', 'woman_and_fence.png']
-INPUT_MASK = ['apple_man_mask.jpg', 'apple_man_mask.jpg', 'apple_man_mask.jpg',
-              'dog_on_bench_mask.png', 'dog_on_bench_mask.png', 'dog_on_bench_mask.png',
-              'flowers_mask.jpg', 'flowers_mask.jpg', 'flowers_mask.jpg',
-              'lebron_mask.jpeg', 'lebron_mask.jpeg', 'lebron_mask.jpeg',
-              'london_mask.jpg', 'london_mask.jpg', 'london_mask.jpg',
-              'statue_mask.jpg', 'statue_mask.jpg', 'statue_mask.jpg',
-              'sunflower_mask.jpg', 'sunflower_mask.jpg', 'sunflower_mask.jpg',
-              'tree_and_building_mask.png', 'tree_and_building_mask.png', 'tree_and_building_mask.png',
-              'two_men_on_bench_mask.png', 'two_men_on_bench_mask.png', 'two_men_on_bench_mask.png',
-              'woman_and_fence_mask.png', 'woman_and_fence_mask.png', 'woman_and_fence_mask.png']
+import os
+
+
+INPUT_IMAGE = ['apple_man.jpg',
+               'dog_on_bench.png',
+               'flowers.jpg',
+               'lebron.jpeg',
+               'london.jpg',
+               'statue.jpeg',
+               'sunflower.jpg',
+               'tree_and_building.png',
+               'two_men_on_bench.png',
+               'woman_and_fence.png']
+
+INPUT_MASK = ['apple_man_mask.jpg',
+              'dog_on_bench_mask.png',
+              'flowers_mask.jpg',
+              'lebron_mask.jpeg',
+              'london_mask.jpg',
+              'statue_mask.jpg',
+              'sunflower_mask.jpg',
+              'tree_and_building_mask.png',
+              'two_men_on_bench_mask.png',
+              'woman_and_fence_mask.png']
+
+
+COMMANDS_PER_IMAGE = 3
 COMMAND = ['Turn the apple into an orange', 'Make the apple a flower', 'Have a bite taken out of the apple',
            'Turn the dog black', 'Make the dog a Walker Hound', 'Remove the dog',
            'Replace the flowers with roses', 'Make the flowers covered in snow', 'Shrink the flowers',
@@ -28,26 +35,26 @@ COMMAND = ['Turn the apple into an orange', 'Make the apple a flower', 'Have a b
            'Change the tree to a cactus', 'Remove the tree', 'Make the tree blue',
            'Give them sandals', 'Make them barefoot', 'Give them cleats',
            'Make her blonde', 'Make her hair straight', 'Make her hair shorter']
-OUTPUT_IMAGE = ['apple_man_orange_{}_{}_{}.jpg', 'apple_man_flower_{}_{}_{}.jpg', 'apple_man_bite_{}_{}_{}.jpg'
-                'dog_on_bench_black_{}_{}_{}.png', 'dog_on_bench_hound_{}_{}_{}.png', 'dog_on_bench_remove_{}_{}_{}.png',
-                'flowers_roses_{}_{}_{}.jpg', 'flowers_snow_{}_{}_{}.jpg', 'flowers_shrink_{}_{}_{}.jpg',
-                'lebron_pumpkin_{}_{}_{}.jpeg', 'lebron_blue_{}_{}_{}.jpeg', 'lebron_remove_{}_{}_{}.jpeg',
-                'london_mountain_{}_{}_{}.jpg', 'london_fireworks_{}_{}_{}.jpg', 'london_cloudy_{}_{}_{}.jpg',
-                'statue_clown_{}_{}_{}.jpeg', 'statue_black_{}_{}_{}.jpeg', 'statue_bald_{}_{}_{}.jpeg',
-                'sunflower_tulip_{}_{}_{}.jpg', 'sunflower_human_face_{}_{}_{}.jpg', 'sunflower_sunglasses_{}_{}_{}.jpg',
-                'tree_and_building_cactus_{}_{}_{}.png', 'tree_and_building_remove_{}_{}_{}.png', 'tree_and_building_blue_{}_{}_{}.png',
-                'two_men_on_bench_sandals_{}_{}_{}.png', 'two_men_on_bench_barefoot_{}_{}_{}.png', 'two_men_on_bench_cleats_{}_{}_{}.png',
-                'woman_and_fence_blonde_{}_{}_{}.png', 'woman_and_fence_straight_{}_{}_{}.png', 'woman_and_fence_short_{}_{}_{}.png']
-VANILLA_OUTPUT = ['apple_man_orange_vanilla.jpg', 'apple_man_flower_vanilla.jpg', 'apple_man_bite_vanilla.jpg',
-                  'dog_on_bench_black_vanilla.png', 'dog_on_bench_hound_vanilla.png', 'dog_on_bench_remove_vanilla.png',
-                  'flowers_roses_vanilla.jpg', 'flowers_snow_vanilla.jpg', 'flowers_shrink_vanilla.jpg',
-                  'lebron_pumpkin_vanilla.jpeg', 'lebron_blue_vanilla.jpeg', 'lebron_remove_vanilla.jpeg',
-                  'london_mountain_vanilla.jpg', 'london_fireworks_vanilla.jpg', 'london_cloudy_vanilla.jpg',
-                  'statue_clown_vanilla.jpeg', 'statue_black_vanilla.jpeg', 'statue_bald_vanilla.jpeg',
-                  'sunflower_tulip_vanilla.jpg', 'sunflower_human_face_vanilla.jpg', 'sunflower_sunglasses_vanilla.jpg',
-                  'tree_and_building_cactus_vanilla.png', 'tree_and_building_remove_vanilla.png', 'tree_and_building_blue_vanilla.png',
-                  'two_men_on_bench_sandals_vanilla.png', 'two_men_on_bench_barefoot_vanilla.png', 'two_men_on_bench_cleats_vanilla.png',
-                  'woman_and_fence_blonde_vanilla.png', 'woman_and_fence_straight_vanilla.png', 'woman_and_fence_short_vanilla.png']
+
+OUTPUT_IMAGE_ALIASES = ['orange', 'flower', 'bite',
+                        'black', 'hound', 'remove',
+                        'roses', 'snow', 'shrink',
+                        'pumpkin', 'blue', 'remove',
+                        'mountain', 'fireworks', 'cloudy',
+                        'clown', 'black', 'bald',
+                        'tulip', 'human_face', 'sunglasses',
+                        'cactus', 'remove', 'blue',
+                        'sandals', 'barefoot', 'cleats',
+                        'blonde', 'straight', 'short']
+
+
+def insert_at_end_of_path(path, addition):
+    extsplit = os.path.splitext(path)
+    return extsplit[0] + '_' + addition + extsplit[1]
+
+
+OUTPUT_IMAGE = [insert_at_end_of_path(INPUT_IMAGE[i // COMMANDS_PER_IMAGE], alias + '_{}_{}_{}') for i, alias in enumerate(OUTPUT_IMAGE_ALIASES)]
+VANILLA_OUTPUT = [insert_at_end_of_path(INPUT_IMAGE[i // COMMANDS_PER_IMAGE], alias + '_vanilla') for i, alias in enumerate(OUTPUT_IMAGE_ALIASES)]
 
 INPUT_DIR = 'inputs/'
 OUTPUT_DIR = 'results/'
